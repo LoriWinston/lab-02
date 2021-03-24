e8fe0db8419d7d6b52534270defb7c3d7f3a2019
Lori Winston <Lcwinston@gmail.com>
2021-03-24 08:57:20 -0700

    all working

--- arrayMethods.js
+++ arrayMethods.js
@@ -3,41 +3,60 @@
 // 2. functions that return functions
 
 const mappedFunction = (array, callback) => {
-    let newArr= [];
-    for ( let i = 0; i < array.length; i++){
-      newArr = [ ...newArr, array[i] + 1] 
+  let newArr= [];
+  for ( let i = 0; i < array.length; i++){
+    newArr = [ ...newArr, array[i] + 1] 
+  }
+
+  return newArr;
+
+};
+
+const filterFunction = (array, callback) => {
+  let tempArr = [];
+
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] % 2 === 0){
+      tempArr = [ ...tempArr, array[i]] 
     }
-    
-    return newArr;
+  }
+  
+  return tempArr;
 
-  };
+};
 
-  const filterFunction = (array, callback) => {
-    let tempArr = [];
+const findIndexFunction = (array, callback) => {
 
-    for(let i = 0; i < array.length; i++) {
-      if(array[i] % 2 === 0) {
-        tempArr = [...tempArr, array[i]]
-      }
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] > 4){
+      return i
     }
-    return tempArr;
-  };
-  
-  const findIndexFunction = (array, callback) => {
-    let tempArr = [];
+  }
 
-    for(let i = 0; i < array.length; i++) {
-      if(array[i] % 2 === 0) {
-        tempArr = [...tempArr, array[i]]
-      }
-    }
-    return tempArr;
-  };
-  
+};
 
+
+const reduceFunction = (array, callback, initialValue) => {
+
+  let accumulator = initialValue
+  for(let item of array) accumulator = callback(accumulator, item);
   
-  module.exports = {
-    mappedFunction,
-    filterFunction
-  };
-  
+  return accumulator;
+}
+
+
+const everyFunction = (array, callback) => {
+for(let item of array) {
+    if (!callback(item)) return false;
+}
+
+return true;
+}
+
+module.exports = {
+  mappedFunction,
+  filterFunction,
+  findIndexFunction,
+  reduceFunction,
+  everyFunction
+};
--- arrayMethods.test.js
+++ arrayMethods.test.js
@@ -1,55 +1,103 @@
 const {
-    filterFunction,
-    mappedFunction,
-    findIndexFunction
-    } = require('./arrayMethods');
-  
-  describe('should use callback function in mappedFunction to get a new mapped array that multiplies number by 2', () => {
-      it('should get old array and return modified array with the callback instructions ', () => {
-  
-        const numbers = [ 1, 2, 3, 4, 5]
-  
-        const actual = mappedFunction(numbers, (i) => i + 1)
-  
-        expect(actual).toEqual([2, 3, 4, 5, 6])
-  
-      });
-  });
+  filterFunction,
+  mappedFunction,
+  findIndexFunction,
+  reduceFunction,
+  everyFunction
+  } = require('./arrayMethods');
 
-  // filter function test
-  describe('should use callback filterFunction to get a new filtered array that also only returns even numbers', () => {
+//MAPPED FUNCTION TEST
+describe('should use callback function in mappedFunction to get a new mapped array that multiplies number by 2', () => {
+    it('should get old array and return modified array with the callback instructions ', () => {
 
-    it('should get old array and return modified array with the callback instructions returning only even numbers', () => {
+      const numbers = [ 1, 2, 3, 4, 5]
 
-      function ifFunction(number){
-        if (number % 2 === 0){
-          return number
-        }
-      }
+      const actual = mappedFunction(numbers, (i) => i + 1)
 
-      const numbers = [1, 2, 3, 4, 5]
+      expect(actual).toEqual([2, 3, 4, 5, 6])
 
-      const actual = filterFunction(numbers, (i) => ifFunction(i) )
-
-      expect(actual).toEqual([2, 4])
     });
-  });
- 
-  // find index function test
+});
 
-  describe('should use callback findIndexFunction to get a new filtered array that returns only even numbers', () => {
+//FILTER FUNCTION TEST
+describe('should use callback filterFunction to get a new FILTERED array that returns only even numbers', () => {
   it('should get old array and return modified array with the callback instructions returning even numbers only', () => {
 
     function ifFunction(number){
-      if (number % 2 === 0) {
+      if (number % 2 === 0){
         return number
       }
     }
 
-    const numbers = [1, 2, 3, 4, 5]
+    const numbers = [ 1, 2, 3, 4, 5]
 
     const actual = filterFunction(numbers, (i) => ifFunction(i))
 
     expect(actual).toEqual([2, 4])
-    });
+
   });
+});
+
+//FIND INDEX FUNCTION TEST
+describe('should use callback findIndexFunction to get INDEX array that returns the index number', () => {
+  it('should return index', () => {
+
+    function ifFunction(number){
+      if (number > 4){
+        return number[i]
+      }
+    }
+
+    const numbers = [ 1, 2, 3, 4, 5]
+
+    const actual = findIndexFunction(numbers, (i) => ifFunction(i))
+
+    expect(actual).toEqual(4)
+
+  });
+});
+
+//REDUCE FUNCTION TEST
+describe('should use callback reduceFunction to get the accumulation of array', () => {
+  it('should return the accumulation', () => {
+
+    const array = [ 1, 2, 3, 4, 5]
+    let initialValue = 2;
+    const callback = (accumulator, item) => accumulator + item
+  
+
+    const actual = reduceFunction(array, callback, initialValue)
+
+    expect(actual).toEqual(17)
+
+  });
+});
+
+//EVERY FUNCTION TEST
+describe('should use callback everyFunction and returns true', () => {
+  it('should return false', () => {
+
+    const array = [ 1, 2, 3, 4, 5]
+
+    const callback = (item) => item === 1
+  
+
+    const actual = everyFunction(array, callback)
+
+    expect(actual).toBeFalsy();
+
+  });
+
+  it('should return true', () => {
+
+    const array = [ 1, 1, 1, 1]
+
+    const callback = (item) => item === 1
+  
+
+    const actual = everyFunction(array, callback)
+
+    expect(actual).toBeTruthy();
+
+  });
+});
